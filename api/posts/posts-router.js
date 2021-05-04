debab6fcd376d5f450a76f14e9b8bbbb3e95de32
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-04 19:58:01 -0400

    client can get post comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -32,7 +32,7 @@ router.post('/', async (req, res) => {
             res.status(400).json({ message: "Please provide title and contents for the post" })
         } else {
             const newPost = await Posts.insert(req.body)
-            res.status(201).json(newPost)
+            res.status(201).json(await Posts.findById(newPost.id))
         }
     } catch {
         res.status(500).json({ message: "There was an error while saving the post to the database" })
@@ -46,7 +46,7 @@ router.put('/:id', async (req, res) => {
         } else {
             const updatedPost = await Posts.update(req.params.id, req.body)
             !updatedPost ? res.status(404).json({ message: "The post with the specified ID does not exist" }) 
-            : res.status(200).json(updatedPost)
+            : res.status(200).json(await Posts.findById(req.params.id))
         }
     } catch {
         res.status(500).json({ message: "The post information could not be modified" })
@@ -55,11 +55,22 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
+        const deletedPostContents = await Posts.findById(req.params.id)
         const deletedPost = await Posts.remove(req.params.id)
         if(!deletedPost) {res.status(404).json({ message: "The post with the specified ID does not exist"})}
-        else res.status(200).json(deletedPost)
+        else res.status(200).json(deletedPostContents)
     } catch {
         res.status(500).json({ message: "The post could not be removed" })
+    }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const postComments = await Posts.findPostComments(req.params.id)
+        postComments.length === 0 ? res.status(404).json({ message: "The post with the specified ID does not exist" })
+        : res.status(200).json(postComments)
+    } catch {
+        res.status(500).json({ message: "The comments information could not be retrieved" })
     }
 })
 
