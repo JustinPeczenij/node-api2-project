c475bd7c75854ca6ea580d2ff6d2c099ba79cab3
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-04 18:59:55 -0400

    client is able to create, update, and delete posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -24,4 +24,43 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body)
+        const postFromClient = req.body
+        if(!postFromClient.title || !postFromClient.contents) {
+            res.status(400).json({ message: "Please provide title and contents for the post" })
+        } else {
+            const newPost = await Posts.insert(req.body)
+            res.status(201).json(newPost)
+        }
+    } catch {
+        res.status(500).json({ message: "There was an error while saving the post to the database" })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({ message: "Please provide title and contents for the post" })
+        } else {
+            const updatedPost = await Posts.update(req.params.id, req.body)
+            !updatedPost ? res.status(404).json({ message: "The post with the specified ID does not exist" }) 
+            : res.status(200).json(updatedPost)
+        }
+    } catch {
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedPost = await Posts.remove(req.params.id)
+        if(!deletedPost) {res.status(404).json({ message: "The post with the specified ID does not exist"})}
+        else res.status(200).json(deletedPost)
+    } catch {
+        res.status(500).json({ message: "The post could not be removed" })
+    }
+})
+
 module.exports = router
